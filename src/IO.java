ca22c9b41430351ec1cd1d84249a1c48dbc577fb
rawalbb <rawalbb@vcu.edu>
2017-11-27 14:10:28 -0500

    Update IO.java

--- src/IO.java
+++ src/IO.java
@@ -1,10 +1,11 @@
+//max waiting time for IO = 50
 import java.util.Random;
 public class IO {
 
         private Random randomIO = new Random();
 
         public int generateIOBurst() {
-            return Clock.getClock() + ((randomIO.nextInt(50)));
+            return OSClock.getClock() + ((randomIO.nextInt(50)));
         }
 
 }
