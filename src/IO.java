fd042aecb5bcedac0657049541f091dc2e4d9c43
rawalbb <rawalbb@vcu.edu>
2017-11-27 14:41:35 -0500

    Update IO.java

--- src/IO.java
+++ src/IO.java
@@ -1,11 +1,34 @@
 //max waiting time for IO = 50
 import java.util.Random;
-public class IO {
+public class IO
+{
+    private boolean busy;
+    private int iofinish;
+    public IO(CPU cpu)
+    {
+        this.cpu = cpu;
+    }
 
-        private Random randomIO = new Random();
+    private Random randomIO = new Random();
 
-        public int generateIOBurst() {
-            return OSClock.getClock() + ((randomIO.nextInt(50)));
+    public int generateIOBurst()
+    {
+        return iofinish = OSClock.getClock() + ((randomIO.nextInt(50))); //double check 50
+        busy = true;
+    }
+
+    public void IOExecution
+    {
+        if (OSClock.getClock >= iofinish && busy)
+        {
+            cpu.interruptProcessor.setFlag(InterruptProcessor.IOComplete);//FIX THIS
+            busy = false;
         }
+    }
+    public boolean IOAvailability()
+    {
+        return busy;
+    }
+        
 
 }
