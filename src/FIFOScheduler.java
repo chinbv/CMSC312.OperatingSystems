3154145824c7354061fffe2b9f62f2d81de37267
Vishakha Sehgal <sehgalv@vcu.edu>
2017-12-01 23:03:20 -0500

    fifo works

--- src/FIFOScheduler.java
+++ src/FIFOScheduler.java
@@ -1,32 +1,38 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class FIFOScheduler  implements OSScheduler{
+public class FIFOScheduler  implements OSScheduler {
 
     ArrayList<CPUCore> allCores = new ArrayList<CPUCore>();
     ProcessControlBlock previouslyAssignedPCB;
-    ArrayList<ProcessControlBlock> ffQueue;
+    public ArrayList<ProcessControlBlock> ffQueue = new ArrayList<>();
     CPUCore aCore;
     ProcessControlBlock nextp;
     Iterator<CPUCore> coreIterator = allCores.iterator();
 
 
-
     public FIFOScheduler() {
-        ffQueue = new ArrayList<>();
+
     }
 
     @Override
     public void schedule() {
 
         allCores = Weeboo.allCores();
-        ffQueue = coreHelperArrayL(Weeboo.processManager().readyQueue());
+        ffQueue = coreHelperArrayL(fifoScheduling(Weeboo.processManager().readyQueue()));
+
+    }
+    public ArrayList<ProcessControlBlock> fifoScheduling(ArrayList<ProcessControlBlock> q)
+    {
+        ffQueue = q;
+        return ffQueue;
     }
 
-    public ArrayList<ProcessControlBlock> coreHelperArrayL(ArrayList<ProcessControlBlock> q)
-    {
-
-        while( coreIterator.hasNext() && q.isEmpty()!=true) {
+    public ArrayList<ProcessControlBlock> coreHelperArrayL(ArrayList<ProcessControlBlock> q) {
+        for (int i = 0; i < q.size(); i++) {
+        }
+        Iterator<CPUCore> coreIterator = allCores.iterator();
+        while (coreIterator.hasNext() && q.isEmpty() != true) {
             aCore = coreIterator.next();
             previouslyAssignedPCB = aCore.assignedProcess();
             if (previouslyAssignedPCB != null) {
@@ -35,25 +41,19 @@ public class FIFOScheduler  implements OSScheduler{
                     aCore.assignProcess(null);
                     previouslyAssignedPCB = null;
                 }
-
             }
 
-            if(previouslyAssignedPCB == null)
-            {
-                if(q.isEmpty()!=true)
-                {
+            if (previouslyAssignedPCB == null) {
+                if (q.isEmpty() != true) {
                     nextp = q.get(0);
                 }
-                if(nextp!=null)
-                {
+                if (nextp != null) {
                     aCore.assignProcess(nextp);
                     nextp.setProcessState(ProcessControlBlock.processState.RUN);
                     q.remove(0);
                 }
             }
         }
-
         return q;
     }
-
 }
